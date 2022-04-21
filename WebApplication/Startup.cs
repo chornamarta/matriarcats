62cc9198108c852f74a55bf03c9cd0d38d1b08bb
chornamarta <80642336+chornamarta@users.noreply.github.com>
2022-04-21 21:49:41 +0300

    Add files via upload

---
+++ WebApplication/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureDbServices(services);
+        }
+        
+        private void ConfigureDbServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt => 
+                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"))
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseSerilogRequestLogging();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
