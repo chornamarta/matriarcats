952e70a20fbc2cc826b54dcc900ea962b4091ea3
UlianaKostetska <80642100+UlianaKostetska@users.noreply.github.com>
2022-05-27 14:29:48 +0300

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using LFC.DAL;
+using LFC.DAL.Models;
+using LFC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace LFC.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly LFCDbContext _db;
+        public AccountController(LFCDbContext db)
+        {
+            _db = db;
+        }
+        //public async Task<IActionResult> Register()
+        //{
+        //    return View(await _db.Courses.Include(c => c.Teachers).ToListAsync());
+        //}
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LFC.DAL;
+using LFC.DAL.Models;
+using LFC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace LFC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly LFCDbContext _db;
+        public HomeController(LFCDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Courses.Include(c => c.Teachers).ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
